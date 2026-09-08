029f6582abbe8a43b2f597be941b84f8b929f994
Vitor Gomes <vitorjgomes25@gmail.com>
2026-09-08 01:12:26 -0300

    Refactor code structure for improved readability and maintainability

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env/env.js'
 import { HttpModule } from './http/http.module.js'
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    HttpModule,
+  ],
 })
 export class AppModule {}
--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module.js'
+import { EnvService } from './env/env.service.js'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  const envService = app.get(EnvService)
+  const port = envService.get('PORT')
+  await app.listen(port)
 }
 await bootstrap()
